2635a6a533a7a25089408a21a279b38a24d850ce
sai00000007 <saibabubarri60@gmail.com>
2025-06-12 23:17:51 +0530

    Add files via upload

---
+++ day1.py/pg1.py
@@ -0,0 +1,3 @@
+name=input('Enter Your Name: ')
+age=int(input('Enter Your Age: '))
+print(f"Hello S{name}. Good Morning!")
---
+++ day1.py/pg2..py
@@ -0,0 +1,8 @@
+n1=int(input("Enter the First Value: "))
+n2=int(input("Enter the Second Value: "))
+n1=n1+n2
+n2=n1-n2
+n1=n1-n2
+print(f"The valuea are swapped succesfully! now N1 = {n1} and N2 = {n2}")
+print(n1)
+print(n2)
